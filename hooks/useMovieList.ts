3d5be6aa379708c76b1da0882277868941d2ba39
pv-oliveira <paulo.victor.ig@hotmail.com>
2023-04-04 21:53:56 -0300

    movie watching component and modal of general info

---
+++ hooks/useMovieList.ts
@@ -0,0 +1,16 @@
+import useSWR from 'swr';
+import fetcher from '@/libs/fetched';
+
+const useMoviesList = () => {
+    const { data, error, isLoading } = useSWR("/api/movies", fetcher, {
+        revalidateIfStale: false,
+        revalidateOnFocus: false,
+        revalidateOnReconnect: false
+    })
+
+    return {
+        data, error, isLoading
+    }
+}
+
+export default useMoviesList;
--- pages/index.tsx
+++ pages/index.tsx
@@ -2,6 +2,12 @@ import { NextPageContext } from "next";
 import { getSession, signOut } from "next-auth/react";
 
 import Navbar from "@/components/Navbar";
+import Bilboard from "@/components/Bilboard";
+import MovieList from "@/components/MovieList";
+import useMoviesList from "@/hooks/useMovieList";
+import useFavorites from "@/hooks/useFavorites";
+import InfoModal from "@/components/InfoModal";
+import useInfoModalStore from "@/hooks/useInfoModal";
 
 export async function getServerSideProps(context: NextPageContext) {
   const session = await getSession(context);
@@ -21,10 +27,19 @@ export async function getServerSideProps(context: NextPageContext) {
 }
 
 export default function Home() {
+  const { data: movies = [] } = useMoviesList();
+  const { data: favorites = [] } = useFavorites();
+  const { isOpen, closeModal } = useInfoModalStore()
 
   return (
     <>
+      <InfoModal visible={isOpen} onClose={closeModal} />
       <Navbar />
+      <Bilboard />
+      <div className="pb-40">
+        <MovieList title="Sucesso" data={movies} />
+        <MovieList title="Minha Lista" data={favorites} />
+      </div>
     </>
   );
 }
